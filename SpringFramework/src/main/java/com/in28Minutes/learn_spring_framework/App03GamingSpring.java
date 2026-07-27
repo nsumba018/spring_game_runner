d853e9bad8e5b2078d5979a66b40767405419d0a
IRAKOZE Nsumba Herve <nsumbaherve11@gmail.com>
2026-07-27 12:15:13 +0200

    Simpler codes

--- SpringFramework/src/main/java/com/in28Minutes/learn_spring_framework/App03GamingSpring.java
+++ SpringFramework/src/main/java/com/in28Minutes/learn_spring_framework/App03GamingSpring.java
@@ -4,12 +4,29 @@ import com.in28Minutes.learn_spring_framework.game.GameRunner;
 import com.in28Minutes.learn_spring_framework.game.GamingConsole;
 import com.in28Minutes.learn_spring_framework.game.PacManGame;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class App03GamingSpring {
+
+    @Bean
+    public GamingConsole game(){
+        var game = new PacManGame();
+        return game;
+    }
+
+    @Bean
+    public GameRunner gameRunner(GamingConsole game){
+        var gameRunner = new GameRunner(game);
+        return gameRunner;
+    }
+
     public static void main(String[] args) {
+        
         try( var context =
                      new AnnotationConfigApplicationContext
-                             (GamingConfiguration.class)){
+                             (App03GamingSpring.class)){
             context.getBean(GamingConsole.class).up();
             context.getBean(GameRunner.class).run();
         }
