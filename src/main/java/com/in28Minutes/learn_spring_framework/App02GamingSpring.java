fb90665d69bbda99a8e5eeb838da6e10720f9a4f
IRAKOZE Nsumba Herve <nsumbaherve11@gmail.com>
2026-07-27 09:50:45 +0200

    Auto Wiring using MethodCall or Parameters, and creating custom names for beans

--- src/main/java/com/in28Minutes/learn_spring_framework/App02GamingSpring.java
+++ src/main/java/com/in28Minutes/learn_spring_framework/App02GamingSpring.java
@@ -16,7 +16,8 @@ public class App02GamingSpring {
         System.out.println(context.getBean("name"));
         System.out.println(context.getBean("age"));
         System.out.println(context.getBean("person"));
-        System.out.println(context.getBean("address2"));
         System.out.println(context.getBean("person2MethodCall"));
+        System.out.println(context.getBean("person3Parameter"));
+        System.out.println(context.getBean("address2"));
     }
 }
--- src/main/java/com/in28Minutes/learn_spring_framework/HelloWorldConfiguration.java
+++ src/main/java/com/in28Minutes/learn_spring_framework/HelloWorldConfiguration.java
@@ -3,7 +3,7 @@ package com.in28Minutes.learn_spring_framework;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-record Person(String name, int age) { };
+record Person(String name, int age, Address address) { };
 
 record Address(String street, String city) { }
 
@@ -21,17 +21,31 @@ public class HelloWorldConfiguration {
 
     @Bean
     public Person person(){
-        return new Person("Ravi", 25);
+        return new Person("Ravi", 25, new Address("Main Street", "Utrech"));
+    }
+    //Auto-wiring using method calls
+    // using already created beans in a new bean as method calls
+    @Bean
+    public Person person2MethodCall(){
+        return new Person(name(),age(), address());
+    }
+    // Auto-wiring using parameters
+    @Bean
+    public Person person3Parameter(String name, int age, Address address3){
+        return new Person(name,age, address3);
     }
 
     @Bean(name = "address2")
     public Address address(){
         return new Address("KK501 ST", "Kigali");
     }
-    //Auto-wiring using method calls
-    // using already created beans in a new bean as method calls
-    
-    // Auto-wiring using parameters
+
+    @Bean(name = "address3") // creating custom names for beans
+    public Address address3(){
+        return new Address("sonatube", "Kicukiro");
+    }
+
+
 
 }
 
