bdb699f95fa744e3433d4d0e5d4bc71a7ed16fef
IRAKOZE Nsumba Herve <nsumbaherve11@gmail.com>
2026-07-27 12:04:22 +0200

    Reorganize project into BasicJava and SpringFramework modules

---
+++ SpringFramework/src/main/java/com/in28Minutes/learn_spring_framework/game/GamingConsole.java
@@ -0,0 +1,8 @@
+package com.in28Minutes.learn_spring_framework.game;
+
+public interface GamingConsole {
+    void up();
+    void down();
+    void left();
+    void right();
+}
---
+++ SpringFramework/src/main/java/com/in28Minutes/learn_spring_framework/game/MarioGame.java
@@ -0,0 +1,19 @@
+package com.in28Minutes.learn_spring_framework.game;
+
+public class MarioGame implements GamingConsole{
+
+    public void up() {
+        System.out.println("Jump");
+    }
+    public void down() {
+        System.out.println("Go into the hole");
+    }
+
+    public void left() {
+        System.out.println("Go Back");
+    }
+
+    public void right() {
+        System.out.println("Go Forward");
+    }
+}
